87a049e8e409760b0cc5be0297e191ede9b84f09
keyzen <alonso.anavalon@gmail.com>
2021-12-06 00:24:23 -0300

    crud producto y clase precio terminada

--- EVA2/main.py
+++ EVA2/main.py
@@ -2,6 +2,7 @@ from EVA2.producto import Producto
 from EVA2.boleta import Boleta
 
 
+
 #Antes de agregarlo deberíamos hacer las comprobaciones correspondientes
 
 def validarNumero(pregunta):
@@ -45,35 +46,75 @@ def generarBoletaVenta(numBoleta):
     txt += "El total de la compra es = ${}".format(total)
     return txt
 
+# CRUD PRODUCTOS
+# Create
+
+def crearProducto(idproducto, nombre, stock, idprecio, preciolista, ganancia):
+    producto = Producto(idproducto,nombre,stock,idprecio,preciolista, ganancia)
+    listaProductos.append(producto)
+    return producto
+
+def traerProducto(idproducto, listaProductos):
+    idBuscar = idproducto
+    for producto in listaProductos:
+        if producto.idproducto == idBuscar:
+            return producto.mostrarProducto()
+
+def actualizarProducto(idproducto, nombre, stock, idprecio, preciolista, ganancia, listaProductos):
+    idBuscar = idproducto
+    for producto in listaProductos:
+        if producto.idproducto == idBuscar:
+            producto.nombre = nombre
+            producto.stock = stock
+            producto.idprecio = idprecio
+            producto.preciolista = preciolista
+            producto.ganancia = (ganancia / 100) + 1
+            producto.precio = producto.ganancia * producto.preciolista
+            return producto.mostrarProducto()
+
+def eliminarProducto(idproducto, listaProductos):
+    idBuscar = idproducto
+    contador = 0
+    for producto in listaProductos:
+        print("Vuelta n°{}".format(contador))
+        if producto.idproducto == idBuscar:
+            listaProductos.pop(contador)
+            print("Se ha eliminado el producto con id {}, contador {}, nombre {}".format(producto.idproducto, contador, producto.nombre))
+            return listaProductos
+            break
+        else:
+            contador += 1
+
+
+
+
+
 listaProductos = []
 
-martillo = Producto(1, "martillo", 3000, 50)
-listaProductos.append(martillo)
-taladro = Producto(2, "taladro", 30000, 50)
-listaProductos.append(taladro)
-cemento = Producto(3, "cemento", 10000, 50)
-listaProductos.append(cemento)
-clavos = Producto(4, "clavos", 3000, 50)
-listaProductos.append(clavos)
-tornillos = Producto(5, "tornillos", 2500, 50)
-listaProductos.append(tornillos)
-tuboPvc = Producto(6, "tubo pvc", 4000, 50)
-listaProductos.append(tuboPvc)
-pintura = Producto(7, "pintura", 6000, 50)
-listaProductos.append(pintura)
-pastaMuro = Producto(8, "pasta muro", 10000, 50)
-listaProductos.append(pastaMuro)
-desatornillador = Producto(9, "desatornillador", 4000, 50)
-listaProductos.append(desatornillador)
-diablo = Producto(10, "diablo", 8000, 50)
-listaProductos.append(diablo)
+martillo = crearProducto(1, "martillo", 50, 2, 8000, 20)
+taladro = crearProducto(2, "taladro", 50, 3, 30000, 15)
+cemento = crearProducto(3, "cemento", 40, 4, 40000, 15)
+clavos = crearProducto(4, "clavos", 50, 5, 3000, 30)
+tornillos = crearProducto(5, "tornillos", 100, 6, 2500, 50)
+tuboPvc = crearProducto(6, "tubo pvc", 60, 7, 4000, 40)
+pintura = crearProducto(7, "pintura", 50, 8, 6000, 30)
+pastaMuro = crearProducto(8, "pasta muro", 60, 9, 10000, 35)
+desatornillador = crearProducto(9, "desatornillador", 30, 10, 4000, 35)
+diablo = crearProducto(10, "diablo", 20, 11, 8000, 20)
+espatula = crearProducto(11, "espatula", 10, 1, 1000, 10)
+
+#Prueba eliminando producto OK, Prueba traerProducto OK, actualizarProducto OK
+#eliminarProducto(10, listaProductos)
+#print(traerProducto(10, listaProductos))
+#actualizarProducto(10,"test", 100, 11, 1000, 20, listaProductos)
+
 
 listaBoleta = []
+
 numBoleta = 1
 productoEncontrado = False
 
 mostrarProductos(listaProductos)
-
 
 while True:
     productoEncontrado = False
@@ -96,13 +137,17 @@ while True:
         print("\nID nuevo producto: ", nuevoProductoId)
         nombre = input("\nIngrese nombre del producto: ")
         nombre = nombre.lower()
-        precio = int(input("Ingrese precio: "))
-        while precio < 1:
-            precio = int(input("Ingrese precio: "))
+        preciolista = int(input("Ingrese precio: "))
+        while preciolista < 1:
+            preciolista = int(input("Ingrese precio: "))
+        ganancia = int(input("Ingrese ganancia: "))
+        while ganancia < 1:
+            ganancia = int(input("Ingrese ganancia: "))
         stock = int(input("Ingrese stock: "))
         while stock < 1:
             stock = int(input("Ingrese stock: "))
-        nuevoProducto = Producto(nuevoProductoId, nombre, precio, stock)
+
+        nuevoProducto = crearProducto(nuevoProductoId, nombre, stock, nuevoProductoId, preciolista, ganancia)
         print("\nNuevo producto agregado: ", nuevoProducto)
         listaProductos.append(nuevoProducto)
 
@@ -120,11 +165,12 @@ while True:
             while idBuscar > len(listaProductos):
                 idBuscar = validarNumero("Ingrese un ID entre {} y {}:".format(1, len(listaProductos)))
             for articulo in listaProductos:
-                if articulo.id == idBuscar:
+                if articulo.idproducto == idBuscar:
 
                     print("Precio a modificar: ", articulo)
                     nuevoPrecio = validarNumero("Ingrese nuevo precio: ")
-                    articulo.actualizarPrecio(nuevoPrecio)
+                    nuevaGanancia = validarNumero("Ingrese un nuevo porcentaje de ganancia en número: ")
+                    articulo.actualizarPrecio(nuevoPrecio, nuevaGanancia)
                     print("Precio modificado: ", articulo)
 
         if buscar == 2:
@@ -165,7 +211,7 @@ while True:
                     idBuscar = validarNumero("Ingrese ID válido entre {} y {}:".format(1, len(listaProductos)))
 
                 for articulo in listaProductos:
-                    if articulo.id == idBuscar:
+                    if articulo.idproducto == idBuscar:
                         productoEncontrado = True
                         if articulo.stock < 1 or articulo.stock == 0:
                             print("No hay stock de {}".format(articulo.nombre))
@@ -273,3 +319,5 @@ while True:
     else:
         print("\nProceso finalizado....")
         break
+
+
---
+++ EVA2/precio.py
@@ -0,0 +1,16 @@
+class Precio():
+    def __init__(self, idprecio, preciolista, ganancia):
+        self.idprecio = idprecio
+        self.preciolista = preciolista
+        self.ganancia = (ganancia / 100) + 1
+        self.precio = self.ganancia * self.preciolista
+
+    def mostrarPrecio(self):
+        txt = "El precio es {:.0f}".format(self.precio)
+        return txt
+
+
+
+
+
+
